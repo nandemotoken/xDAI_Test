1908bcf97660c72f74c58b449e1759f38df6f077
nandemotoken <nandemotoken@gmail.com>
2020-05-16 15:24:20 +0000

    index

--- app.js
+++ app.js
@@ -7,5 +7,59 @@ const web3 = new Web3(portis.provider)
 web3.eth.getAccounts((error, accounts) => {
     console.log(accounts);
   });
-  
+
+const Address = "0x589A65f3dA3Cf199cd4382F53DA0C819cB8c2859";
+
+//remixのボタンを押して取得したABIを貼り付け
+const ABI = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "n",
+        "type": "uint256"
+      }
+    ],
+    "name": "setnumber",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "get3",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getnumber",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+
+mycontract = new web3js.eth.Contract(ABI, Address);
+
+mycontract.methods.get3().call().then((fromblockchain1) => {
+    console.log("get3 answer is " + fromblockchain1);
+  });
+
 }
