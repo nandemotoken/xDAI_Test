a91793672903a3bbf135ede8429b1a4cf66ea081
nandemotoken <nandemotoken@gmail.com>
2020-05-16 14:20:58 +0000

    index

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ window.onload = ()=> {
 portis = new Portis('6ae05414-fd51-41d5-bd20-db8d944e86a9', 'xdai')
 web3 = new Web3(portis.provider)
 
+web3.eth.getAccounts((error, accounts) => {
+    console.log(accounts);
+  });
+  
 }
