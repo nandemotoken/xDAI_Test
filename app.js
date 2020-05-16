8640e56e42739aeb3b4faf76c6784ac65fd61678
nandemotoken <nandemotoken@gmail.com>
2020-05-16 14:57:57 +0000

    index

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const portis;
-const web3;
 
 window.onload = ()=> {
 
-portis = new Portis('6ae05414-fd51-41d5-bd20-db8d944e86a9', 'xdai')
-web3 = new Web3(portis.provider)
+const portis = new Portis('6ae05414-fd51-41d5-bd20-db8d944e86a9', 'xdai')
+const web3 = new Web3(portis.provider)
 
 web3.eth.getAccounts((error, accounts) => {
     console.log(accounts);
